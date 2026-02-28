b70ed1e967829dc9a6b801c2521fe2fde3d48f9b
zero123-max <zerubbabel.mercado@gmail.com>
2026-02-28 10:51:53 +0800

    Update and rename README.md to Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using IMissHerThoughtsProcessing.Views;
+
+namespace IMissHerThoughtsProcessing
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            StringView stringView = new StringView();
+            stringView = showMenu();
+        }
+    }
+}
